60876bcdbe41d1689ddda9a3182eb93d3b48022a
mhoelzer <mhoelzer@pio.carrollu.edu>
2017-07-10 15:44:51 -0400

    try try try

---
+++ router.js
@@ -0,0 +1,7 @@
+// make stuff for videso or w/e
+// the app will refer to the express from the index.js
+module.exports = function(app){
+	app.get('/', function(req, res){
+		res.send("hhhhheyeyeyeyeyooooooo")
+	})
+}
