313e0459dacc3f241a8ebadb70e1c5ed154c4cad
mhoelzer <mhoelzer@pio.carrollu.edu>
2017-07-24 13:21:03 -0400

    finished

--- router.js
+++ router.js
@@ -1,7 +1,8 @@
 // has all routes that will be hit to send users plaes
 // directs data to send to controlers, but first need to send data
 const Auth = require('./controllers/auth');
-const User = require('./models/user')
+// const User = require('./models/user');
+const BucketList = require('./controllers/bucketlistcontroller');
 const passportService = require('./services/passport');
 const passport = require('passport');
 
@@ -23,4 +24,11 @@ module.exports = function(app){
 	// Auth is considered an object b/c of the . notation
 	app.post('/signup', Auth.signup)
 	app.post('/signin', requireSignin, Auth.signin);
+	// requireAuth is going to make sure that the post goes through passport and the JWT token gets checked
+	//  last parameter calls the addBucketList function on the BucketList variable. This will allow us to save our new post to the db. 
+	app.post('/newitem', requireAuth, BucketList.addBucketList);
+	app.get('/items', requireAuth, BucketList.fetchBucketLists);
+	app.get('/items/:id', requireAuth, BucketList.fetchBucketList);
+	app.put('/items/:id', requireAuth, BucketList.updateBucketList);
+	app.delete('/items/:id', requireAuth, BucketList.deleteBucketList);
 }
